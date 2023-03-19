947a232e688062d9cd7d8d17572999ad380b23d8
Erick Luiz Bertolini <erick.luiz.bertolini@gmail.com>
2023-03-19 11:24:41 -0300

    feature: speed/image

--- components/product/ProductImages.tsx
+++ components/product/ProductImages.tsx
@@ -1,37 +1,49 @@
-import { ImageObject } from "https://denopkg.com/deco-sites/std@0.1.4/commerce/types.ts";
-import { useState } from 'preact/hooks'
+import { useState } from "preact/hooks";
 import Slider from "$store/components/ui/Slider.tsx";
+import Image from "deco-sites/std/components/Image.tsx";
 
+export default function ProductImages(
+  { productImages }: { productImages: ImageObject[] },
+) {
+  const [imageSelected, setImageSelected] = useState<ImageObject>(
+    productImages[0],
+  );
 
-
-export default function ProductImages({ productImages }: { productImages: ImageObject[] }) {
-
-    const [imageSelected, setImageSelected] = useState<ImageObject>(productImages[0])
-  
-    return (
-      <div className="w-full md:max-w-[60%]">
-        <div className="w-[635] h-[635] flex justify-center hover:overflow-hidden">
-          <img
-            src={imageSelected.url}
-            alt={imageSelected.alternateName}
-            className="hover:scale-150"
-            style={{
-              width: 635,
-              height: 635
-            }}
-          />
-        </div>
-        <div className="px-2 py-1">
-            <Slider>
-              { productImages.map((imageOfProduct)=>{
-                return (
-                  <button className="border" onClick={()=>setImageSelected(imageOfProduct)}>
-                    <img src={imageOfProduct.url} alt={imageOfProduct.alternateName} />
-                  </button>
-                )
-              }) }
-            </Slider>
-        </div>
+  return (
+    <div className="w-full md:max-w-[60%]">
+      <div className="w-[635] h-[635] flex justify-center hover:overflow-hidden">
+        <img
+          src={imageSelected.url}
+          alt={imageSelected.alternateName}
+          className="hover:scale-150"
+          height={64}
+          width={64}
+          style={{
+            width: 635,
+            height: 635,
+          }}
+        />
       </div>
-    );
-  }
+      <div className="px-2 py-1">
+        <Slider>
+          {productImages.map((imageOfProduct) => {
+            return (
+              <button
+                className="border"
+                onClick={() => setImageSelected(imageOfProduct)}
+              >
+                <Image
+                  src={imageOfProduct.url}
+                  alt={imageOfProduct.alternateName}
+                  width={64}
+                  height={64}
+                  class="rounded w-full group-hover:hidden"
+                />
+              </button>
+            );
+          })}
+        </Slider>
+      </div>
+    </div>
+  );
+}
